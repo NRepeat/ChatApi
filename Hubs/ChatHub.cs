07fe5509d73f5caffdd92ae2a3531467edb3d389
Mykyta Nazarov <nnazarov55@gmail.com>
2024-11-17 01:10:14 +0200

    added redis, override OnDisconnectedAsync chat hub function

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Caching.Distributed;
+using System.Collections.Specialized;
 using System.Text.Json;
 using WebApi.Models;
 namespace WebApi.Hubs;
 
 public interface IChatClient
 {
-    public Task ReceiveMessage(string userName, string message);
+    public Task ReceiveAdminMessage(string userName, string message);
+    public Task ReceiveClientMessage(string userName, string message);
 }
 
 
@@ -23,17 +25,30 @@ public class ChatHub : Hub<IChatClient>
         await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatRoom);
         string stringConnection = JsonSerializer.Serialize(connection);
         await _cashe.SetStringAsync(Context.ConnectionId,stringConnection);
-        await Clients.Group(connection.ChatRoom).ReceiveMessage("Client", $"{connection.User} connected");
+        await Clients.Group(connection.ChatRoom).ReceiveAdminMessage("Admin", connection.ChatRoom);
     }
     public async Task SendMessage(string message)
     {
         var stringConnection = await _cashe.GetAsync(Context.ConnectionId);
         var connection =  JsonSerializer.Deserialize<UserConnection>(stringConnection);
+        Console.WriteLine(connection);
         if(connection is not null)
         {
-            await Clients.Group(connection.ChatRoom).ReceiveMessage(connection.User, message);
+            await Clients.Group(connection.ChatRoom).ReceiveClientMessage(connection.User, message);
         }
   
+    }
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        var stringConnection = await _cashe.GetAsync(Context.ConnectionId);
+        var connection = JsonSerializer.Deserialize<UserConnection>(stringConnection);
+        if (connection is not null)
+        {
+            await _cashe.RemoveAsync(Context.ConnectionId);
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, connection.ChatRoom);
+                 await Clients.Group(connection.ChatRoom).ReceiveAdminMessage("Admin", $"{connection.User} disconnected");
+        }
+        await base.OnDisconnectedAsync(exception);
     }
 }
 
