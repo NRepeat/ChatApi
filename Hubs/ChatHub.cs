900354077c23fa4aa064320340339ec7b2efc46c
Mykyta Nazarov <nnazarov55@gmail.com>
2024-11-16 23:30:19 +0200

    initialize redis cashe, add SendMessage function

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,24 +1,41 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
 using WebApi.Models;
-
-
-
-
 namespace WebApi.Hubs;
 
 public interface IChatClient
 {
-    public Task ReciveMessage(string userName, string message);
+    public Task ReceiveMessage(string userName, string message);
 }
 
 
 public class ChatHub : Hub<IChatClient>
 {
+    private readonly IDistributedCache _cashe;
+    public ChatHub(IDistributedCache cashe)
+    {
+        _cashe = cashe;
+    }
+
     public async Task JoinChat(UserConnection connection)
     {
         await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatRoom);
-        await Clients.Group(connection.ChatRoom).ReciveMessage("Client", $"{connection.User} connected");
+        string stringConnection = JsonSerializer.Serialize(connection);
+        await _cashe.SetStringAsync(Context.ConnectionId,stringConnection);
+        await Clients.Group(connection.ChatRoom).ReceiveMessage("Client", $"{connection.User} connected");
+    }
+    public async Task SendMessage(string message)
+    {
+        var stringConnection = await _cashe.GetAsync(Context.ConnectionId);
+        var connection =  JsonSerializer.Deserialize<UserConnection>(stringConnection);
+        if(connection is not null)
+        {
+            await Clients.Group(connection.ChatRoom).ReceiveMessage(connection.User, message);
+        }
+  
     }
 }
+
 
 
--- Program.cs
+++ Program.cs
@@ -2,13 +2,17 @@ using WebApi.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    var connection = builder.Configuration.GetConnectionString("Redis");
+    options.Configuration = connection;
+});
+
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
@@ -21,7 +25,6 @@ builder.Services.AddCors(options =>
 });
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
